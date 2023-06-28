15596aef25aec332d2cd0a4710d598445b709899
Elian M <elian060893@gmail.com>
2023-06-28 17:30:21 -0300

    mod js verificação email

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const button = document.querySelector(".button-submit");
-
 const form = document.querySelector(".formulary");
+const validRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+const email = document.querySelector("#email-to-newsletters");
+const verifyEmail = document.querySelector("#has-error");
+const validEmail = validRegex.test(email.value) === true;
+button.addEventListener("click", ()=>{
+   
+    form.addEventListener("submit", (e) => { 
+        e.preventDefault();
+        if (!email.value || validEmail)
+        {
+            verifyEmail.style.display = "flex";
+            email.classList.add("has-error");
+         }
+         else 
+         {
+            alert("Email Validated with succes");
+         }
+    })
+})
+email.addEventListener("input", ()=>{
 
-form.addEventListener("submit", (e) => { 
-    e.preventDefault();
-    const email = document.querySelector("#email-to-newsletters");
-    const verifyEmail = document.querySelector("#has-error");
-    if (!email.value)
-    {
-        verifyEmail.style.display = "flex";
-        email.classList.add("has-error")
-     }
+    verifyEmail.style.display = "none";
+    email.classList.remove("has-error");
+
 })
