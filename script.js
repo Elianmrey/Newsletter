586cb8245f4b51bfe2d0682418c20892c820e83a
Elian M Reyes <elian060893@gmail.com>
2023-06-29 20:11:44 -0300

    Mudanças não salvadas

--- script.js
+++ script.js
@@ -3,19 +3,24 @@ const form = document.querySelector(".formulary");
 const validRegex = /^[\w-_.]+@[\w-_.]+\.[\w]{2,}/gi;
 const email = document.querySelector("#email-to-newsletters");
 const verifyEmail = document.querySelector("#has-error");
-const validEmail = validRegex.test(email.value);
+validRegex.test(email.value);
 button.addEventListener("click", ()=>{
    
     form.addEventListener("submit", (e) => { 
         e.preventDefault();
-        if (!email.value || validEmail === false)
+        if (!email.value || validRegex.test(email.value) === false)
         {
             verifyEmail.style.display = "flex";
             email.classList.add("has-error");
+            console.log("Testando")
          }
-         else 
+         else if(validRegex.test(email.value) === true)
          {
+
+            console.log("Testando else")
             alert("Email Validated with succes");
+            verifyEmail.style.display = "none";
+            email.classList.remove("has-error");
          }
     })
 })
