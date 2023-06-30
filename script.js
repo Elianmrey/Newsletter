9f6cb91b14e2930262d6220e62e0b9ad734a9eb4
Elian M Reyes <elian060893@gmail.com>
2023-06-29 23:08:53 -0300

    Mods

--- script.js
+++ script.js
@@ -4,25 +4,25 @@ const validRegex = /^[\w-_.]+@[\w-_.]+\.[\w]{2,}/gi;
 const email = document.querySelector("#email-to-newsletters");
 const verifyEmail = document.querySelector("#has-error");
 validRegex.test(email.value);
-button.addEventListener("click", ()=>{
-   
-    form.addEventListener("submit", (e) => { 
-        e.preventDefault();
-        if (!email.value || validRegex.test(email.value) === false)
-        {
-            verifyEmail.style.display = "flex";
-            email.classList.add("has-error");
-            console.log("Testando")
-         }
-         else if(validRegex.test(email.value) === true)
-         {
 
-            console.log("Testando else")
-            alert("Email Validated with succes");
-            verifyEmail.style.display = "none";
-            email.classList.remove("has-error");
-         }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
     })
+
+button.addEventListener("click", () => { 
+    
+    if (!email.value || validRegex.test(email.value) === false) {
+        verifyEmail.style.display = "flex";
+        email.classList.add("has-error");
+        console.log("Testando")
+    }
+    else {
+        console.log("Testando else")
+        alert("Email Validated with succes");
+        verifyEmail.style.display = "none";
+        email.classList.remove("has-error");
+    }
 })
 email.addEventListener("input", ()=>{
 
