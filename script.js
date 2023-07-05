e807c70f702553ec72491914eba1854769a67345
Elian M Reyes <elian060893@gmail.com>
2023-07-04 22:19:01 -0300

    mod

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const form = document.querySelector(".formulary");
 const validRegex = /^[\w-_.]+@[\w-_.]+\.[\w]{2,}/gi;
 const email = document.querySelector("#email-to-newsletters");
 const verifyEmail = document.querySelector("#has-error");
+
 validRegex.test(email.value);
 
 
@@ -30,3 +31,22 @@ email.addEventListener("input", ()=>{
     email.classList.remove("has-error");
 
 })
+
+const title = `Thanks for Subscribing`
+const message = `A confirmation email has sent to ${email.value}. Please open it an click the button inside to confirm your subscription`;
+
+class SuccessWindow
+{
+    constructor(title,message){
+        this.title = title;
+        this.message = message;
+}
+}
+const divRefill = new SuccessWindow(title, message);
+const divMessage = document.querySelector(".advise-message");
+divMessage.display = "flex";
+divMessage.innerHTML = `
+<h2 class ="window-title">${divRefill.title}</h2>
+<p class = "confirmation-message">${divRefill.message}</p>
+<button>Dismiss Message`;
+
