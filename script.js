69ba6c2f0be4871a7166e2fe163dc7cc85065ba8
Elian M Reyes <elian060893@gmail.com>
2023-07-08 16:31:01 -0300

    modr

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const successMessage = document.querySelector(".advise-message");
 const subscriptionSection = document.querySelector(".news-subscribtion-container");
 
 const title = `Thanks for Subscribing`
-const message = subscribeNewEmail();
+const message = "";
 const  icon = `<svg class="icon-success" xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>`;
 
 
@@ -44,9 +44,9 @@ const  icon = `<svg class="icon-success" xmlns="http://www.w3.org/2000/svg" widt
 
            const captureEmail =  subscribeNewEmail();
             const regTest = validRegex.test(email.value);
-            console.log(`Captured `)
+        // console.log(`Captured email: ${captureEmail}`)
          
-        if (captureEmail[0] ==="" || regTest === false) {
+        if (email.value ==="" || regTest === false) {
                 verifyEmail.style.display = "flex";
                 email.classList.add("has-error");
 
@@ -57,13 +57,16 @@ const  icon = `<svg class="icon-success" xmlns="http://www.w3.org/2000/svg" widt
                 email.classList.remove("has-error");
                 subscriptionSection.style.display = "none";
                 subscriptionSection.style.userSelect = "none";
-                successMessage.style.display = "flex";            
-        }
+                successMessage.style.display = "flex";
+                divRefill[message] = captureEmail;            
+        } 
+      
     }
 
 
 
 button.addEventListener("click", verificaEmail);
+
 email.addEventListener("input", ()=>{
     verifyEmail.style.display = "none";
     email.classList.remove("has-error");
@@ -84,10 +87,14 @@ const divRefill = new SuccessWindow(title, message, icon);
 successMessage.innerHTML = `
 <div class="svg-container">${divRefill.icon}</div>
 <h2 class ="window-title">${divRefill.title}</h2>
-<p class = "confirmation-message">${divRefill.message}</p>
+<p class = "confirmation-message"></p>
 <button class="button-dismiss">Dismiss Message</button>`;
 
+
+const appendParagraph = document.querySelector(".confirmation-message");
 const buttonDismiss = document.querySelector(".button-dismiss");
+appendParagraph.append(divRefill.message)
+
 
 buttonDismiss.addEventListener("click", () => { 
 
