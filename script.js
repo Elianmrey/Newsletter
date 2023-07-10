0f4bf0cc52aab834081b511a3106eaa19d63e5f3
Elian M <elian060893@gmail.com>
2023-07-10 16:46:01 -0300

    Email no Advise message funcionando

--- script.js
+++ script.js
@@ -5,27 +5,26 @@ const email = document.querySelector("#email-to-newsletters");
 const verifyEmail = document.querySelector("#has-error");
 
 
-const validRegex = /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}/gim;
+const validRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/ig;
 const validValue =  validRegex.test(email.value);
 const emailValue = [];
 
 //Working here
-function subscribeNewEmail() {
-         
-    emailValue.splice(0,1);
-        emailValue.push(email.value);
-       
-        console.log(emailValue);
-        const text = `A confirmation email has sent to ${emailValue[0]}. Please open it an click the button inside to confirm your subscription`;
-        return text;
-    }; 
-
 const successMessage = document.querySelector(".advise-message");
 const subscriptionSection = document.querySelector(".news-subscribtion-container");
-
 const title = `Thanks for Subscribing`
-const message = subscribeNewEmail();
 const  icon = `<svg class="icon-success" xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64"><defs><linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%"><stop offset="0%" stop-color="#FF6A3A"/><stop offset="100%" stop-color="#FF527B"/></linearGradient></defs><g fill="none"><circle cx="32" cy="32" r="32" fill="url(#a)"/><path stroke="#FFF" stroke-width="4" d="m18.286 34.686 8.334 7.98 19.094-18.285"/></g></svg>`;
+
+
+class SuccessWindow
+{
+    constructor(title, icon){
+        this.icon = icon;
+        this.title = title;
+      
+}
+}
+const divRefill = new SuccessWindow(title, icon);
 
 
 // Listener do Submit para preventDefault() e validar
@@ -37,7 +36,7 @@ const  icon = `<svg class="icon-success" xmlns="http://www.w3.org/2000/svg" widt
 
     function verificaEmail(){
 
-            subscribeNewEmail();
+          
             const regTest = validRegex.test(email.value);
             
          
@@ -52,8 +51,24 @@ const  icon = `<svg class="icon-success" xmlns="http://www.w3.org/2000/svg" widt
                 email.classList.remove("has-error");
                 subscriptionSection.style.display = "none";
                 subscriptionSection.style.userSelect = "none";
-                successMessage.style.display = "flex";            
-        }
+                successMessage.style.display = "flex"; 
+                
+successMessage.innerHTML = `
+<div class="svg-container">${divRefill.icon}</div>
+<h2 class ="window-title">${divRefill.title}</h2>
+<p class = "confirmation-message"> A confirmation email has sent to <span class="to-email"> ${email.value}</span>. Please open it an click the button inside to confirm your subscription </p>
+<button class="button-dismiss">Dismiss Message</button>`;
+        const buttonDismiss = document.querySelector(".button-dismiss");
+
+buttonDismiss.addEventListener("click", () => { 
+
+    subscriptionSection.style.display = "flex";
+    subscriptionSection.style.pointerEvents = "all";
+    subscriptionSection.style.userSelect = "none";
+    successMessage.style.display = "none";
+     email.value = "";
+});
+}
     }
 
 
@@ -74,29 +89,5 @@ email.addEventListener("input", ()=>{
 
 
 
-class SuccessWindow
-{
-    constructor(title,message, icon){
-        this.icon = icon;
-        this.title = title;
-        this.message = message;
-}
-}
-const divRefill = new SuccessWindow(title, message, icon);
 
-successMessage.innerHTML = `
-<div class="svg-container">${divRefill.icon}</div>
-<h2 class ="window-title">${divRefill.title}</h2>
-<p class = "confirmation-message">${divRefill.message}</p>
-<button class="button-dismiss">Dismiss Message</button>`;
 
-const buttonDismiss = document.querySelector(".button-dismiss");
-
-buttonDismiss.addEventListener("click", () => { 
-
-    subscriptionSection.style.display = "flex";
-    subscriptionSection.style.pointerEvents = "all";
-    subscriptionSection.style.userSelect = "none";
-    successMessage.style.display = "none";
-     email.value = "";
-})
